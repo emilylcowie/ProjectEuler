504714f9baac374bbe821ba58ed3401a6872a084
emilylcowie <emilylcowie@gmail.com>
2025-02-04 22:14:40 +0000

    Refactor Multiples-of-3-or-5.py and Largest-Prime -Factor.py for clarity and efficiency

--- 1. Multiples-of-3-or-5.py
+++ 1. Multiples-of-3-or-5.py
@@ -3,26 +3,31 @@
 #   Find the sum of all the multiples of 3 or 5 below 1000 "
 
 
-
+# ----------------------------------------------------------------------
 # simplest way to solve for range of 1000
-sum1 = 0 
+
+sum1 = 0
 limit = 1000
 for i in range(limit):
-    if i%3 == 0 or i%5 == 0:
+    if i % 3 == 0 or i % 5 == 0:
         sum1 += i
 print(sum1)
 
-
+# ----------------------------------------------------------------------
 
 # more efficient method to instead calculate the sum of numbers divisible
-# by 3, add to the sum of numbers divisible by 5, then subtract sum of 
-# numbs divisible by 15:
-def sum_div_by(n):
-    p = (limit-1) // n
-    return n*(p*(p+1)) // 2
+#   by 3, add to the sum of numbers divisible by 5, then subtract sum of
+#   nums divisible by 15:
 
-print(sum_div_by(3) + sum_div_by(5) - sum_div_by(15))
 
+def sum_divisible_by(divisor):
+    max_multiple = (limit - 1) // divisor
+    return divisor * (max_multiple * (max_multiple + 1)) // 2
+
+
+print(sum_divisible_by(3) + sum_divisible_by(5) - sum_divisible_by(15))
+
+# ----------------------------------------------------------------------
 
 # using list comprehension
-print(sum([x for x in range(1000) if x % 3== 0 or x % 5== 0]))
+print(sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0]))
--- 3. Largest-Prime-Factor.py
+++ 3. Largest-Prime-Factor.py
@@ -1,16 +1,20 @@
 # " The prime factors of 13195 are 5, 7, 13 and 29.
 #   What is the largest prime factor of the number 600851475143? "
 
-number = 600851475143
-factors = []
-prime_factors = []
+def largest_prime_factor(number):
+    factor = 2
+    while factor ** 2 <= number:
+        if number % factor:
+            factor += 1
+        else:
+            number //= factor
+    return number
 
-for i in range(round(number/3),0,-1):
-    print(i)
-    if number % i == 0:
-        factors.append(i)
-        break
-        
-print(factors)
+num = 600851475143
+print(largest_prime_factor(num))
 
-#unfinished
+# Explanation:
+#   Continue dividing the number by the smallest factor until factor squared is greater than the number.
+#   If the number is not divisible by the current factor, increment the factor
+#   If the number is divisible by the current factor, divide the number by the factor
+#   The remaining number is the largest prime factor
