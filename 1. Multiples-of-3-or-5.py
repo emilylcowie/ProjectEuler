b9efff922de2a33fdfe9258ecf94591a77e1163c
Emily Cowie <emilylcowie@gmail.com>
2025-02-04 00:38:43 +0000

    finished 1 and 2 did further research on 1

--- 1. Multiples-of-3-or-5.py
+++ 1. Multiples-of-3-or-5.py
@@ -1,1 +1,28 @@
+# " If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9.
+#   The sum of these multiples is 23.
+#   Find the sum of all the multiples of 3 or 5 below 1000 "
 
+
+
+# simplest way to solve for range of 1000
+sum1 = 0 
+limit = 1000
+for i in range(limit):
+    if i%3 == 0 or i%5 == 0:
+        sum1 += i
+print(sum1)
+
+
+
+# more efficient method to instead calculate the sum of numbers divisible
+# by 3, add to the sum of numbers divisible by 5, then subtract sum of 
+# numbs divisible by 15:
+def sum_div_by(n):
+    p = (limit-1) // n
+    return n*(p*(p+1)) // 2
+
+print(sum_div_by(3) + sum_div_by(5) - sum_div_by(15))
+
+
+# using list comprehension
+print(sum([x for x in range(1000) if x % 3== 0 or x % 5== 0]))
---
+++ 3. Largest-Prime-Factor.py
@@ -0,0 +1,16 @@
+# " The prime factors of 13195 are 5, 7, 13 and 29.
+#   What is the largest prime factor of the number 600851475143? "
+
+number = 600851475143
+factors = []
+prime_factors = []
+
+for i in range(round(number/3),0,-1):
+    print(i)
+    if number % i == 0:
+        factors.append(i)
+        break
+        
+print(factors)
+
+#unfinished
