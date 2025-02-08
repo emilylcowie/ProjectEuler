6a04033131e66dc1cae33138ca0c281c9210b218
Emily Cowie <emilylcowie@gmail.com>
2025-02-08 23:54:54 +0000

    Update 7. 1001'st-Prime-Number.py

--- 7. 1001'st-Prime-Number.py
+++ 7. 1001'st-Prime-Number.py
@@ -1,7 +1,7 @@
 # " By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
 #       we can see that the 6th prime is 13.
 
-#       What is the 1001'st prime number? "
+#      What is the 1001'st prime number? "
 
 def is_prime(n):
     for i in range(2,n):
