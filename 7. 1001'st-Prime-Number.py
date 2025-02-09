dd6864d78e1f084cb93feff8c7288d288e2b5180
emilylcowie <emilylcowie@gmail.com>
2025-02-09 22:02:02 +0000

    Refactor prime number calculation to find the 10001'st prime number

--- 7. 1001'st-Prime-Number.py
+++ 7. 1001'st-Prime-Number.py
@@ -1,19 +1,21 @@
 # " By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
 #       we can see that the 6th prime is 13.
 
-#      What is the 1001'st prime number? "
+#      What is the 10001'st prime number? "
 
 def is_prime(n):
-    for i in range(2,n):
-        print("I",i)
-        print("N",n)
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            print("True")
-        else:
-            print("False")
             return False
     return True
-        
-for n in range(1,100):
-    if is_prime(n) == True:
-        print("")
+
+count = 0
+num = 1
+while count < 10001:
+    num += 1
+    if is_prime(num):
+        count += 1
+
+print(num)
